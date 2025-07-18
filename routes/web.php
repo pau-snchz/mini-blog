d22b739e99978198d443e42a3d6dcb3052515b06
pau-snchz <96534030+pau-snchz@users.noreply.github.com>
2025-07-18 16:59:39 +0800

    modify routing

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\LikedBlogsController;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/blog/create', [BlogController::class, 'create'])->name('blog.create');
+    Route::post('/blog', [BlogController::class, 'store'])->name('blog.store');
+    Route::get('/blog/{id}/edit', [BlogController::class, 'edit'])->name('blog.edit');
+    Route::put('/blog/{id}', [BlogController::class, 'update'])->name('blog.update');
+    Route::delete('/blog/{id}', [BlogController::class, 'destroy'])->name('blog.destroy');
+});
+
+Route::get('/blog/{id}', [BlogController::class, 'show'])->name('blog.show');
+Route::post('/blog/{id}/comment', [BlogController::class, 'storeComment'])->name('blog.comment');
+Route::post('/blog/{id}/like', [BlogController::class, 'toggleLike'])->name('blog.like');
+Route::delete('/comment/{id}', [BlogController::class, 'destroyComment'])->name('comment.destroy')->middleware('auth');
+
+Route::get('/liked-blogs', [LikedBlogsController::class, 'index'])->name('liked.blogs')->middleware('auth');
+Route::delete('/liked-blogs/{id}', [LikedBlogsController::class, 'removeLike'])->name('liked.blogs.remove')->middleware('auth');
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
